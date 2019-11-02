76c22fedd9b1157d171c631cfe503afbbb42965d
shon <shon.sanches@gmail.com>
2019-11-02 19:09:57 +0000

    Created the Register endpoint

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://sonia:soniaMERN@mern-csguy.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://sonia:soniaMERN@mern-csguy.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 }
